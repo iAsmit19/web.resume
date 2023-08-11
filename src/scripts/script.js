899e1bc1a492b10e3557dfcd642b9f2e86ec0c0c
Asmit <iasmiti19@gmail.com>
2023-08-11 21:26:06 +0530

    Updates

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+function clickToCopyPhoneNumber() {
+    navigator.clipboard.writeText("+91 8920688832");
+};
+function clickToCopyEmail() {
+    navigator.clipboard.writeText("iamasmit1919@gmail.com");
+};
